c44eed699ccac3eea65cf7971106d23b07d0b8d4
Renato <renato.junior@dcc.ufmg.br>
2019-05-19 20:58:09 -0300

    COMMIT #5

--- src/tdd/Money.java
+++ src/tdd/Money.java
@@ -1,8 +1,8 @@
 package tdd;
 
-public abstract class Money {
+public class Money {
 	protected int amount;
-	private String currency; 
+	private String currency;
 
 	Money(int amount, String currency) {
 		this.amount = amount;
@@ -10,20 +10,22 @@ public abstract class Money {
 	}
 
 	static Money dollar(int amount) {
-		return new Dollar(amount, "USD");
+		return new Money(amount, "USD");
 	}
 
 	static Money franc(int amount) {
-		return new Franc(amount, "CHF");
+		return new Money(amount, "CHF");
 	}
 
-	abstract Money times(int multiplier);
+	Money times(int multiplier) {
+		return new Money(amount * multiplier, currency);
+	}
 
 	public boolean equals(Object object) {
 		Money money = (Money) object;
-		return amount == money.amount && getClass().equals(money.getClass());
+		return amount == money.amount && currency().equals(money.currency());
 	}
-	
+
 	String currency() {
 		return currency;
 	}
--- src/tdd/Tests.java
+++ src/tdd/Tests.java
@@ -1,6 +1,8 @@
 package tdd;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
@@ -11,4 +13,11 @@ public class Tests {
 		assertEquals("USD", Money.dollar(1).currency());
 		assertEquals("CHF", Money.franc(1).currency());
 	}
+
+	public void testEquality() {
+		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+		assertFalse(Money.franc(5).equals(Money.dollar(5)));
+	}
+
 }
