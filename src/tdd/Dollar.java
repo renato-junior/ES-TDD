c73c69f9e69b07fe1eb57a4103b46249acc3bc9f
Renato <renato.junior@dcc.ufmg.br>
2019-05-19 20:24:43 -0300

    COMMIT #2

--- src/tdd/Dollar.java
+++ src/tdd/Dollar.java
@@ -1,11 +1,13 @@
 package tdd;
 
-public class Dollar {
-	int amount = 10;
+public class Dollar extends Money {
 
 	Dollar(int amount) {
+		this.amount = amount;
 	}
 
-	void times(int multiplier) {
+	Dollar times(int multiplier) {
+		return new Dollar(amount * multiplier);
 	}
+
 }
